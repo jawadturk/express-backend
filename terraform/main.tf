355d6686546a378b814b6fec53ad741b20d59a74
Jawad <jawad.turk@hotmail.com>
2024-01-15 23:33:29 -0500

    trigger dev

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,4 @@
-resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-random-001122-22"
-  location = "us-central1"
-}
-
-resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-random-00112323"
-  location = "us-central1"
+resource "google_service_account" "service_account" {
+  account_id   = "my-sa-1"
+  display_name = "Service Account One"
 }
