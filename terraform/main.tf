79174df1d35ab323d9268145c8166479a07852ca
Jawad <jawad.turk@hotmail.com>
2024-02-03 20:34:38 -0500

    added cloud run service

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,15 @@ resource "google_service_account" "service_account" {
   account_id   = "my-sa-1"
   display_name = "Service Account One"
 }
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloudrun-service"
+  location = "us-central1"
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-central1-docker.pkg.dev/express-backend-411116/container-images/express-backend:1.0.1"
+    }
+  }
+}
